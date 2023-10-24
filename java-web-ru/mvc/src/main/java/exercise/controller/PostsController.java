00eff69dde6f2e93cddbad3a51fa8bb08d866e73
@hexlet/cli <support@hexlet.io>
2023-10-24 20:47:45 +0300

    submit java-web-ru/mvc

---
+++ java-web-ru/mvc/src/main/java/exercise/controller/PostsController.java
@@ -0,0 +1,106 @@
+package exercise.controller;
+
+import java.util.*;
+
+import exercise.dto.posts.PostsPage;
+import exercise.dto.posts.PostPage;
+import exercise.model.Post;
+import exercise.repository.PostRepository;
+import exercise.dto.posts.BuildPostPage;
+import exercise.dto.posts.EditPostPage;
+import exercise.util.NamedRoutes;
+
+import io.javalin.http.Context;
+import io.javalin.validation.ValidationError;
+import io.javalin.validation.ValidationException;
+import io.javalin.http.NotFoundResponse;
+
+public class PostsController {
+
+    public static void build(Context ctx) {
+        var page = new BuildPostPage();
+        ctx.render("posts/build.jte", Collections.singletonMap("page", page));
+    }
+
+    public static void create(Context ctx) {
+        try {
+            var name = ctx.formParamAsClass("name", String.class)
+                    .check(value -> value.length() >= 2, "Название не должно быть короче двух символов")
+                    .get();
+
+            var body = ctx.formParamAsClass("body", String.class)
+                    .check(value -> value.length() >= 10, "Пост должен быть не короче 10 символов")
+                    .get();
+
+            var post = new Post(name, body);
+            PostRepository.save(post);
+            ctx.redirect(NamedRoutes.postsPath());
+
+        } catch (ValidationException e) {
+            var name = ctx.formParam("name");
+            var body = ctx.formParam("body");
+            var page = new BuildPostPage(name, body, e.getErrors());
+            ctx.render("posts/build.jte", Collections.singletonMap("page", page)).status(422);
+        }
+    }
+
+    public static void index(Context ctx) {
+        var posts = PostRepository.getEntities();
+        var postPage = new PostsPage(posts);
+        ctx.render("posts/index.jte", Collections.singletonMap("page", postPage));
+    }
+
+    public static void show(Context ctx) {
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+        var post = PostRepository.find(id)
+                .orElseThrow(() -> new NotFoundResponse("Post not found"));
+
+        var page = new PostPage(post);
+        ctx.render("posts/show.jte", Collections.singletonMap("page", page));
+    }
+
+    // BEGIN
+    public static void editShow(Context ctx) {
+
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+        Post post = PostRepository.find(id).get();
+
+        Map<String, List<ValidationError<Object>>> errors = new HashMap<>();
+
+        var page = new EditPostPage(post.getName(), post.getBody(), id, errors);
+        ctx.render("posts/edit.jte", Collections.singletonMap("page", page));
+
+    }
+    public static void edit(Context ctx) {
+
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+
+        try {
+            var name = ctx.formParamAsClass("name", String.class)
+                    .check(value -> value.length() >= 2, "Название не должно быть короче двух символов")
+                    .get();
+
+            var body = ctx.formParamAsClass("body", String.class)
+                    .check(value -> value.length() >= 10, "Пост должен быть не короче 10 символов")
+                    .get();
+
+            Post post = PostRepository.find(id).get();
+            post.setBody(body);
+            post.setName(name);
+            PostRepository.save(post);
+            ctx.redirect(NamedRoutes.postsPath());
+
+        } catch (ValidationException e) {
+            var name = ctx.formParam("name");
+            var body = ctx.formParam("body");
+            var page = new EditPostPage(name, body, id, e.getErrors());
+            ctx.status(422);
+            ctx.render("posts/edit.jte", Collections.singletonMap("page", page)).status(422);
+        }
+//
+//
+//        var page = new EditPostPage();
+//        ctx.render("posts/edit.jte", Collections.singletonMap("page", page));
+    }
+    // END
+}
