fb8e44a54c317b1d15c664fedcba552336c6e505
@hexlet/cli <support@hexlet.io>
2023-10-28 13:36:56 +0300

    submit java-web-ru/database

---
+++ java-web-ru/database/src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+-- BEGIN
+DROP TABLE IF EXISTS products;
+
+CREATE TABLE products (
+                          id INT PRIMARY KEY AUTO_INCREMENT,
+                          title VARCHAR(255) NOT NULL,
+                          price INTEGER
+);
+
+-- END
