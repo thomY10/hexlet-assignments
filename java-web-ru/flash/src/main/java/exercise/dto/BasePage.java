0277a085f1969cbf309e649d00d3e3ebca9d9bc5
@hexlet/cli <support@hexlet.io>
2023-10-28 12:41:18 +0300

    submit java-web-ru/flash

---
+++ java-web-ru/flash/src/main/java/exercise/dto/BasePage.java
@@ -0,0 +1,11 @@
+package exercise.dto;
+import lombok.Getter;
+import lombok.Setter;
+
+// BEGIN
+@Getter
+@Setter
+public class BasePage {
+    private String flash;
+}
+// END
---
+++ java-web-ru/flash/src/main/java/exercise/dto/posts/PostsPage.java
@@ -0,0 +1,16 @@
+package exercise.dto.posts;
+
+import java.util.List;
+import exercise.model.Post;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import exercise.dto.BasePage;
+
+// BEGIN
+@AllArgsConstructor
+@Getter
+public class PostsPage extends BasePage {
+
+    private List<Post> posts;
+}
+// END
